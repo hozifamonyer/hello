9a9dd019664b9702c9a03adc6bcf9a8703a97f7c
hozifamonyer <110229353+hozifamonyer@users.noreply.github.com>
2023-06-14 05:36:43 -0700

    frist

---
+++ Untitled-3.js
@@ -0,0 +1,13 @@
+window.alert("ok"); //اظهار رسالة للمتسخدم
+window.alert("ok1");
+window.alert("ok2");
+
+
+let x=25;
+let y=5;
+document.getElementById("ss").innerHTML = "hello world";   //
+console.log(x ** y);
+console.log(x+y);
+console.log(x/y);
+console.log(x%y);
+console.log(x-y);
