cc63f1b4a7d6f9fc2b8c698433e34bd059095330
hozifamonyer <110229353+hozifamonyer@users.noreply.github.com>
2023-06-17 05:42:34 -0700

    ero

--- Untitled-3.js
+++ Untitled-3.js
@@ -1,13 +1,31 @@
 window.alert("ok"); //اظهار رسالة للمتسخدم
-window.alert("ok1");
-window.alert("ok2");
+
 
 
 let x=25;
 let y=5;
-document.getElementById("ss").innerHTML = "hello world";   //تعديل او اضافة من html عن طريق اتصال id
 console.log(x ** y);
 console.log(x+y);
 console.log(x/y);
 console.log(x%y);
 console.log(x-y);
+
+let cardss={
+    Header : "hello",
+    content : " I AM HOIFA",
+    footer : "SAVE PAGE"
+
+};
+document.getElementById("card-header").innerHTML = cardss.Header;
+document.getElementById("card-body").innerHTML = cardss.content;
+document.getElementById("card-footer").innerHTML = cardss.footer;
+
+const names=["hos","hoz","hoo"]; 
+const lasts=["mon","moi","mou"];
+
+document.getElementById("frist name1").innerHTML=names[0];
+document.getElementById("lats1").innerHTML=lasts[0];
+document.getElementById("frist name2").innerHTML=names[1];
+document.getElementById("lats2").innerHTML=lasts[1];
+document.getElementById("frist name3").innerHTML=names[2];
+document.getElementById("lats3").innerHTML=lasts[2];
